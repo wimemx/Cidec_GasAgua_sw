c6accbb7b4cefb0c75444f1f80f0dcf13181a066
Osvaldo Bulos Ramírez <bulos@wime.com.mx>
2012-08-20 18:31:52 -0500

    Gráficas con llamadas ajax y correción de fechas

--- c_center/views.py
+++ c_center/views.py
@@ -1,5 +1,5 @@
 #standard library imports
-from datetime import date
+from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 import re
 import time
@@ -15,7 +15,7 @@ from rbac.models import Operation, DataContextPermission
 from rbac.rbac_functions import  has_permission
 from c_center.calculations import tarifaHM
 from c_center.models import ConsumerUnit
-import simplejson
+import json as simplejson
 
 
 VIEW = Operation.objects.get(operation_name="view")
@@ -105,15 +105,58 @@ def potencia_activa(request):
                 template_vars['compare_interval_kw'] = get_KW(building_compare, f1_init, f1_end)
                 if f2_init:
                     template_vars['compare_interval2_kw'] = get_KW(building_compare, f2_init, f2_end)
-    template_vars['main_interval_kw'], template_vars['fi'], template_vars['ff']=get_KW(request.session['main_building'], f1_init, f1_end)
+    template_vars['building']=request.session['main_building'].pk
+    template_vars['fi'], template_vars['ff'] = f1_init, f1_end
+    #template_vars['main_interval_kw'], \
+    #template_vars['fi'], \
+    #template_vars['ff'] = get_KW(request.session['main_building'], f1_init, f1_end)
+
     if f2_init:
         template_vars['main_interval2_kw'] = get_KW(request.session['main_building'], f2_init, f2_end)
 
     template_vars_template = RequestContext(request, template_vars)
     return render_to_response("consumption_centers/graphs/potencia_activa.html", template_vars_template)
 
-def potencia_activa_test(request):
-    template_vars = {"type":"kw"}
+def get_kw_data(request):
+    if 'building' in request.GET:
+        building = get_object_or_404(Building, pk=int(request.GET['building']))
+        f1_init, f1_end = get_intervals_1(request.GET)
+        data=get_KW_json(building, f1_init, f1_end)
+        return HttpResponse(content=data,content_type="application/json")
+    else:
+        raise Http404
+
+
+def get_kvar_data(request):
+    if 'building' in request.GET:
+        building = get_object_or_404(Building, pk=int(request.GET['building']))
+        f1_init, f1_end = get_intervals_1(request.GET)
+        data=get_KVar_json(request.session['main_building'], f1_init, f1_end)
+        return HttpResponse(content=data,content_type="application/json")
+    else:
+        raise Http404
+
+def get_pf_data(request):
+    if 'building' in request.GET:
+        building = get_object_or_404(Building, pk=int(request.GET['building']))
+        f1_init, f1_end = get_intervals_1(request.GET)
+        data=get_PF_json(request.session['main_building'], f1_init, f1_end)
+        return HttpResponse(content=data,content_type="application/json")
+    else:
+        raise Http404
+
+def get_pp_data(request):
+    if 'building' in request.GET:
+        building = get_object_or_404(Building, pk=int(request.GET['building']))
+        f1_init, f1_end = get_intervals_1(request.GET)
+        data=get_power_profile_json(request.session['main_building'], f1_init, f1_end)
+        return HttpResponse(content=data,content_type="application/json")
+    else:
+        raise Http404
+
+
+def potencia_reactiva(request):
+    template_vars = {"type":"kvar"}
 
     #second interval, None by default
     f2_init = None
@@ -142,48 +185,12 @@ def potencia_activa_test(request):
         template_vars['main_interval2_kw'] = get_KW(request.session['main_building'], f2_init, f2_end)
 
     template_vars_template = RequestContext(request, template_vars)
-    return render_to_response("consumption_centers/graphs/test_graph.html", template_vars_template)
-
-def get_kw_data(request):
-    if 'building' in request.GET:
-        building = get_object_or_404(Building, pk=int(request.GET['building']))
-        f1_init, f1_end = get_intervals_1(request.GET)
-        data=get_KW_json(request.session['main_building'], f1_init, f1_end)
-        return HttpResponse(content=data,content_type="application/json")
-    else:
-        raise Http404
-
-
-def potencia_reactiva(request):
-    template_vars = {"type":"kvar"}
-    #second interval, None by default
-    f2_init = None
-    f2_end = None
-
-    f1_init, f1_end = get_intervals_1(request.GET)
-
-    if request.GET:
-
-        if "f2_init" in request.GET:
-            f2_init, f2_end = get_intervals_2(request.GET)
-
-        for key in request.GET:
-            if re.search('^compare_to\d+', key):
-                # "compare", request.session['main_building'], "with building", key
-                building_compare = Building.objects.get(pk=int(key))
-                template_vars['compare_interval_kvar'] = get_KVar(building_compare, f1_init, f1_end)
-                if f2_init:
-                    template_vars['compare_interval2_kvar'] = get_KVar(building_compare, f2_init, f2_end)
-
-    template_vars['main_interval_kvar']=get_KVar(request.session['main_building'], f1_init, f1_end)
-    if f2_init:
-        template_vars['main_interval2_kvar'] = get_KVar(request.session['main_building'], f2_init, f2_end)
-    print template_vars
-    template_vars_template = RequestContext(request, template_vars)
     return render_to_response("consumption_centers/graphs/potencia_reactiva.html", template_vars_template)
+
 
 def factor_potencia(request):
     template_vars = {"type":"pf"}
+
     #second interval, None by default
     f2_init = None
     f2_end = None
@@ -198,19 +205,24 @@ def factor_potencia(request):
             if re.search('^compare_to\d+', key):
                 # "compare", request.session['main_building'], "with building", key
                 building_compare = Building.objects.get(pk=int(key))
-                template_vars['compare_interval_pf'] = get_PF(building_compare, f1_init, f1_end)
+                template_vars['compare_interval_kw'] = get_KW(building_compare, f1_init, f1_end)
                 if f2_init:
-                    template_vars['compare_interval2_pf'] = get_PF(building_compare, f2_init, f2_end)
+                    template_vars['compare_interval2_kw'] = get_KW(building_compare, f2_init, f2_end)
+    template_vars['building']=request.session['main_building'].pk
+    template_vars['fi'], template_vars['ff'] = f1_init, f1_end
+    #template_vars['main_interval_kw'], \
+    #template_vars['fi'], \
+    #template_vars['ff'] = get_KW(request.session['main_building'], f1_init, f1_end)
 
-    template_vars['main_interval_pf']=get_PF(request.session['main_building'], f1_init, f1_end)
     if f2_init:
-        template_vars['main_interval2_pf'] = get_PF(request.session['main_building'], f2_init, f2_end)
-    print template_vars
+        template_vars['main_interval2_kw'] = get_KW(request.session['main_building'], f2_init, f2_end)
+
     template_vars_template = RequestContext(request, template_vars)
     return render_to_response("consumption_centers/graphs/factor_potencia.html", template_vars_template)
 
 def perfil_carga(request):
     template_vars = {"type":"profile"}
+
     #second interval, None by default
     f2_init = None
     f2_end = None
@@ -225,22 +237,21 @@ def perfil_carga(request):
             if re.search('^compare_to\d+', key):
                 # "compare", request.session['main_building'], "with building", key
                 building_compare = Building.objects.get(pk=int(key))
-                template_vars['compare_interval_pf'] = get_PF(building_compare, f1_init, f1_end)
-                template_vars['compare_interval_kvar'] = get_KVar(building_compare, f1_init, f1_end)
                 template_vars['compare_interval_kw'] = get_KW(building_compare, f1_init, f1_end)
                 if f2_init:
-                    template_vars['compare_interval2_pf'] = get_PF(building_compare, f2_init, f2_end)
-                    template_vars['compare_interval2_kvar'] = get_KVar(building_compare, f2_init, f2_end)
                     template_vars['compare_interval2_kw'] = get_KW(building_compare, f2_init, f2_end)
-    template_vars['main_interval_pf'] = get_PF(request.session['main_building'], f1_init, f1_end)
-    template_vars['main_interval_kvar_kw'] = get_power_profile(request.session['main_building'], f1_init, f1_end)
+    template_vars['building']=request.session['main_building'].pk
+    template_vars['fi'], template_vars['ff'] = f1_init, f1_end
+    #template_vars['main_interval_kw'], \
+    #template_vars['fi'], \
+    #template_vars['ff'] = get_KW(request.session['main_building'], f1_init, f1_end)
 
     if f2_init:
-        template_vars['main_interval2_pf'] = get_PF(request.session['main_building'], f2_init, f2_end)
-        template_vars['main_interval_kvar_kw2'] = get_power_profile(request.session['main_building'], f2_init, f2_end)
+        template_vars['main_interval2_kw'] = get_KW(request.session['main_building'], f2_init, f2_end)
 
     template_vars_template = RequestContext(request, template_vars)
     return render_to_response("consumption_centers/graphs/perfil_carga.html", template_vars_template)
+
 
 def set_default_building(request, id_building):
     request.session['main_building'] = Building.objects.get(pk=id_building)
@@ -252,13 +263,12 @@ def set_default_building(request, id_building):
 def recibocfe(request):
 
     #Obtiene los registros de un medidor en un determinado periodo de tiempo
-    pr_powermeter = 2
-    region = 2
-    start_date = '2012-01-01 00:00:00'
-    end_date = '2012-07-31 23:59:59'
+    start_date, end_date = get_intervals_1(request.GET)
+    consumer_unit = ConsumerUnit.objects.get(building=request.session['main_building'])
+    pr_powermeter = ProfilePowermeter.objects.get(pk=consumer_unit.profile_powermeter.pk)
+    region = request.session['main_building'].region
 
-    print tarifaHM(pr_powermeter, start_date, end_date, region)
-
+    vars=dict(tarifa=tarifaHM(pr_powermeter, start_date, end_date, region))
 
     variables = RequestContext(request, vars)
     return render_to_response('consumption_centers/cfe.html', variables)
@@ -276,7 +286,7 @@ def get_medition_in_time(building, datetime_from, datetime_to):
     consumer_unit = ConsumerUnit.objects.get(building=building)
     profile_powermeter = ProfilePowermeter.objects.get(pk=consumer_unit.profile_powermeter.pk)
     meditions = ElectricData.objects.filter(profile_powermeter=profile_powermeter,
-        medition_date__gte=datetime_from,medition_date__lte=datetime_to).order_by("medition_date")
+        medition_date__gte=datetime_from,medition_date__lte=datetime_to+timedelta(days=1)).order_by("medition_date")
     print "medition lenght", len(meditions)
     return meditions
 
@@ -285,7 +295,6 @@ def get_KW(building, datetime_from, datetime_to):
     meditions = get_medition_in_time(building, datetime_from, datetime_to)
     kw=[]
     fi = None
-    ff = None
     for medition in meditions:
         if not fi:
             fi=medition.medition_date
@@ -311,6 +320,15 @@ def get_KVar(building, datetime_from, datetime_to):
 
     return kvar
 
+def get_KVar_json(building, datetime_from, datetime_to):
+    """ Gets the KVar data in a given interval"""
+    meditions = get_medition_in_time(building, datetime_from, datetime_to)
+    kvar=[]
+    for medition in meditions:
+        kvar.append(dict(kvar=str(medition.kvar), date=int(time.mktime(medition.medition_date.timetuple()))))
+
+    return simplejson.dumps(kvar)
+
 
 def get_PF(building, datetime_from, datetime_to):
     """ Gets the KW data in a given interval"""
@@ -319,6 +337,16 @@ def get_PF(building, datetime_from, datetime_to):
     for medition in meditions:
         pf.append(dict(pf=medition.PF, date=medition.medition_date))
     return pf
+
+def get_PF_json(building, datetime_from, datetime_to):
+    """ Gets the PF data in a given interval"""
+    meditions = get_medition_in_time(building, datetime_from, datetime_to)
+    pf=[]
+    for medition in meditions:
+        pf.append(dict(pf=str(medition.PF), date=int(time.mktime(medition.medition_date.timetuple()))))
+
+    return simplejson.dumps(pf)
+
 
 def get_power_profile(building, datetime_from, datetime_to):
     """ gets the data from the active and reactive energy for a building in a time window"""
@@ -329,3 +357,16 @@ def get_power_profile(building, datetime_from, datetime_to):
         kw.append(dict(kw=medition.kW, date=medition.medition_date))
         kvar.append(dict(kvar=medition.kvar, date=medition.medition_date))
     return zip(kvar,kw)
+
+
+def get_power_profile_json(building, datetime_from, datetime_to):
+    """ gets the data from the active and reactive energy for a building in a time window"""
+    meditions = get_medition_in_time(building, datetime_from, datetime_to)
+    #kvar=[]
+    kw=[]
+    for medition in meditions:
+        kw.append(dict(kw=str(medition.kW), kvar=str(medition.kvar), date=int(time.mktime(medition.medition_date.timetuple()))))
+        #kvar.append(dict(kvar=str(medition.kvar), date=int(time.mktime(medition.medition_date.timetuple()))))
+    return simplejson.dumps(kw)
+
+
