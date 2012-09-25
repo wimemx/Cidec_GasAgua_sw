a34575f761ac9a7e86d70636de25725c1b5082db
hector <hector@wime.com.mx>
2012-09-25 18:59:10 -0500

    add roles, front end

--- rbac/urls.py
+++ rbac/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^add_permissions/', 'rbac.views.add_data_context_permissions'),
     url(r'^new_role/', 'rbac.views.add_role'),
+    url(r'^get_group/(?P<id_operation>\d+)', 'rbac.views.get_select_group'),
+    url(r'^get_object/(?P<id_group>\d+)', 'rbac.views.get_select_object'),
 )
--- rbac/views.py
+++ rbac/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.simple import direct_to_template
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponse
 from django.template.context import RequestContext
+from django.http import Http404
 
 from rbac.models import *
 from rbac.rbac_functions import has_permission, get_buildings_context
@@ -17,32 +18,54 @@ def add_role(request):
             pass
         else:
             datacontext = get_buildings_context(request.user)
-            template_vars = dict(datacontext=datacontext, empresa=request.session[
-                                                                  'main_building'])
+            template_vars = dict(datacontext=datacontext,
+                                 empresa=request.session['main_building'],
+                                 operations=Operation.objects.all())
             template_vars_template = RequestContext(request, template_vars)
             return render_to_response("rbac/add_role.html", template_vars_template)
     else:
         return render_to_response("generic_error.html", RequestContext(request))
 
+def get_select_group(request, id_operation):
+    operation_group = OperationForGroup.objects.filter(operation__pk=id_operation)
+    string_to_return=''
+    for operation in operation_group:
+        string_to_return += """<li rel="%s">
+                                %s
+                            </li>""" % (operation.group.pk, operation.group.group_name)
+    return HttpResponse(content=string_to_return, content_type="text/html")
+
+def get_select_object(request, id_group):
+    if request.GET['operation']:
+        objects = OperationForGroupObjects.objects.filter(operation__pk=request.GET[
+                                                                        'operation'],
+                                                          group_object__group__pk=id_group)
+        string_to_return = ''
+        for object in objects:
+            string_to_return+="""
+                            <li>
+                                <input type="checkbox" name="object_%s" id="object_%s"/>
+                                <label for="object_%s" >
+                                    %s
+                                </label>
+                            </li>
+                            """ % (object.group_object.object.pk,
+                                   object.group_object.object.pk,
+                                   object.group_object.object.pk,
+                                   object.group_object.object.object_name)
+        return HttpResponse(content=string_to_return, content_type="text/html")
+    else:
+        raise Http404
+
 def add_data_context_permissions(request):
     """Permission Asigments
     show a form for data context permission asigment
     """
-    if has_permission(request.user, CREATE, "data_context_permissions"):
+    if has_permission(request.user, CREATE, "Asignar roles a usuarios"):
         #has perm to view graphs, now check what can the user see
-        datacontext = get_buildings_context(request.user)
 
-        set_default_session_vars(request, datacontext)
-        #valid years for reporting
-        request.session['years'] = [__date.year
-                                    for __date in
-                                    ElectricData.objects.all().dates('medition_date', 'year')]
 
-        template_vars = {"type":"graphs", "datacontext":datacontext,
-                         'empresa': request.session['main_building'],
-                         'consumer_unit': request.session['consumer_unit']
-        }
-        template_vars_template = RequestContext(request, template_vars)
-        return render_to_response("consumption_centers/main.html", template_vars_template)
+        template_vars_template = RequestContext(request, {})
+        return render_to_response("rbac/add_data_context_permissions.html", template_vars_template)
     else:
         return render_to_response("generic_error.html", RequestContext(request))
