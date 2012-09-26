1278385872f2cd4d615dff04fd76f6cec199b76d
hector <hector@wime.com.mx>
2012-09-26 18:56:27 -0500

    agregar roles completado, empezado listado de roles

--- rbac/views.py
+++ rbac/views.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+import re
+
 from django.views.generic.simple import direct_to_template
-from django.shortcuts import render_to_response, HttpResponse
+from django.shortcuts import render_to_response, HttpResponse, HttpResponseRedirect
 from django.template.context import RequestContext
 from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
+from django.db.models.aggregates import Count
 
 from rbac.models import *
 from rbac.rbac_functions import has_permission, get_buildings_context
@@ -10,12 +16,107 @@ CREATE = Operation.objects.get(operation_name="Crear")
 DELETE = Operation.objects.get(operation_name="Eliminar")
 UPDATE = Operation.objects.get(operation_name="Modificar")
 
+def save_perm(role, objs_ids, operation):
+    """Add a PermissionAsigment for a given role
+    role = a Role instance
+    objs_ids = array with ids of objects
+    operation = string ["ver", "Crear", "Eliminar"], if neither, defaults UPDATE
+    """
+    if operation == "Ver":
+        operation = VIEW
+    elif operation == "Crear":
+        operation = CREATE
+    elif operation == "Eliminar":
+        operation = DELETE
+    else:
+        operation = UPDATE
 
+
+    for obj_id in objs_ids:
+        if obj_id != "all":
+            try:
+                object = Object.objects.get(pk=int(obj_id))
+            except ObjectDoesNotExist:
+
+                mensaje = "El privilegio no existe, por favor seleccione nuevamente la " \
+                          "operaci&oacute;n y el privilegio"
+                return False, mensaje
+            else:
+                perm=PermissionAsigment(role=role, operation=operation,
+                    object=object)
+                perm.save()
+    return True, "El registro se completó exitosamente"
 def add_role(request):
     """Add role web form"""
     if has_permission(request.user, CREATE, "crear rol"):
         if request.method == "POST":
-            pass
+            role = request.POST['role_name'].strip()
+            role_desc = request.POST['role_desc'].strip()
+            rol = Role.objects.filter(role_name__iexact=role)
+            mensaje = ''
+            ntype = ''
+            if rol:
+                asignation = False
+                mensaje = "El rol ya existe, por favor edita el existente o crea uno nuevo"
+            else:
+                rol = Role(role_name=role, role_description=role_desc, role_importance="average" )
+                rol.save()
+                if has_permission(request.user, CREATE, "Asignacion de privilegios"):
+                    asignation = False
+                    for key in request.POST:
+                        objs_ids = request.POST[str(key)].split(",")
+
+                        #checks the type of the allowed operation for the role
+                        if re.search('^Ver_\w+', key):
+                            asignation, mensaje = save_perm(rol, objs_ids, "Ver")
+
+                        elif re.search('^Crear_\w+', key):
+                            asignation, mensaje = save_perm(rol, objs_ids, "Crear")
+
+                        elif re.search('^Eliminar_\w+', key):
+                            asignation, mensaje = save_perm(rol, objs_ids, "Eliminar")
+
+                        elif re.search('^Modificar_\w+', key):
+                            asignation, mensaje = save_perm(rol, objs_ids, "Modificar")
+                else:
+                    asignation = True
+                    mensaje = "Debido a tus privilegios, solo se ha dado de alta el rol"
+                    ntype = "notif"
+
+            if asignation:
+                #if save_perm register the PermissionAsigment correctly
+                if not ntype:
+                    ntype = "success"
+                if has_permission(request.user, VIEW, "Ver roles"):
+                    return HttpResponseRedirect("/panel_de_control/roles?msj=" + mensaje +
+                                                "&ntype=notif")
+                else:
+                    #regresa al formulario de alta
+                    datacontext = get_buildings_context(request.user)
+                    template_vars = dict(datacontext=datacontext,
+                        empresa=request.session['main_building'],
+                        operations=Operation.objects.all(),
+                        message=mensaje,
+                        msg_type="success"
+                    )
+                    template_vars_template = RequestContext(request, template_vars)
+                    return render_to_response("rbac/add_role.html", template_vars_template)
+            else:
+                #regresa al formulario de alta con el mensaje de error,
+                # borro el rol y los privilegios asociados
+                PermissionAsigment.objects.filter(role=rol).delete()
+                rol.delete()
+                datacontext = get_buildings_context(request.user)
+                template_vars = dict(datacontext=datacontext,
+                    empresa=request.session['main_building'],
+                    operations=Operation.objects.all(),
+                    message=mensaje,
+                    msg_type="fail",
+                    post=request.POST
+                )
+                template_vars_template = RequestContext(request, template_vars)
+                return render_to_response("rbac/add_role.html", template_vars_template)
+
         else:
             datacontext = get_buildings_context(request.user)
             template_vars = dict(datacontext=datacontext,
@@ -23,6 +124,37 @@ def add_role(request):
                                  operations=Operation.objects.all())
             template_vars_template = RequestContext(request, template_vars)
             return render_to_response("rbac/add_role.html", template_vars_template)
+    else:
+        return render_to_response("generic_error.html", RequestContext(request))
+
+def view_roles(request):
+    if has_permission(request.user, VIEW, "Ver roles"):
+
+        lista = PermissionAsigment.objects.all().values(
+            "role__pk", "role__role_name","role__role_description").annotate(Count("role"))
+        print lista
+        paginator = Paginator(lista, 6) # muestra 10 resultados por pagina
+        template_vars = dict(roles=paginator)
+        # Make sure page request is an int. If not, deliver first page.
+        try:
+            page = int(request.GET.get('page', '1'))
+        except ValueError:
+            page = 1
+
+        # If page request (9999) is out of range, deliver last page of results.
+        try:
+            pag_role = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            pag_role = paginator.page(paginator.num_pages)
+
+        template_vars['paginacion']=pag_role
+
+        if 'msj' in request.GET:
+            template_vars['message'] = request.GET['msj']
+            template_vars['msg_type'] = request.GET['ntype']
+
+        template_vars_template = RequestContext(request, template_vars)
+        return render_to_response("rbac/role_list.html", template_vars_template)
     else:
         return render_to_response("generic_error.html", RequestContext(request))
 
@@ -44,8 +176,8 @@ def get_select_object(request, id_group):
         for object in objects:
             string_to_return+="""
                             <li>
-                                <input type="checkbox" name="object_%s" id="object_%s"/>
-                                <label for="object_%s" >
+                                <input type="checkbox" name="object_%s" id="%s"/>
+                                <label for="%s" >
                                     %s
                                 </label>
                             </li>
