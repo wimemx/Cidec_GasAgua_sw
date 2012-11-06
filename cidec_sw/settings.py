879f41b7e1475fcbc2b6c3436ed8da5de9f46c16
hector <hector@wime.com.mx>
2012-11-06 10:54:00 -0600

    settings con logger, sin migrations

--- cidec_sw/settings.py
+++ cidec_sw/settings.py
@@ -41,14 +41,14 @@ MANAGERS = ADMINS
     }
 }"""
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': 'satest_cidec',                      # Or path to database file if using sqlite3.
-    'USER': 'root',                      # Not used with sqlite3.
-    'PASSWORD': '',                  # Not used with sqlite3.
-    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-}
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'satest_cidec',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 # Local time zone for this installation. Choices can be found here:
