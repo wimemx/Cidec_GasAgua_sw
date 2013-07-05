1dd9936d2b659249af5956b84aa59e827abefc79
hector <hector@wime.com.mx>
2013-07-05 18:19:56 -0500

    Correcciones en las funciones de restaurar datos desde CSV

--- reports/reports_functions.py
+++ reports/reports_functions.py
@@ -576,7 +576,7 @@ def get_data_statistics(
         "median": data_cluster_median,
         "standard_deviation": data_cluster_standard_deviation
     }
-
+    print statistics
     return statistics
 
 
